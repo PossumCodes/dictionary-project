2d4f1016b69ee52f893d2f72c3de7bb2f3f95a7a
PossumCodes <117069636+PossumCodes@users.noreply.github.com>
2023-08-03 16:16:26 +1000

    added heading

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h1>Lana's Word Lookup</h1>
+        </header>
         <main>
           <Dictionary />
         </main>
