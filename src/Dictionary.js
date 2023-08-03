21d3709429240f15fcf3f78a993795deecb68296
PossumCodes <117069636+PossumCodes@users.noreply.github.com>
2023-08-03 16:05:37 +1000

    add dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -6,12 +7,23 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    //handling response from api and logging api response
+    function handleResponse(response) {
+      console.log(response.data[0]);
+    }
+
+    //documentation: https://dictionaryapi.dev/ making api call
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
+  //event listener tracking change in typing input in search bar
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
+  //dictionary search form
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
