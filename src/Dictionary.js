8ce46ecb8bb141a497a0a6a346c84f36581b449c
PossumCodes <117069636+PossumCodes@users.noreply.github.com>
2023-08-04 16:12:33 +1000

    rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    //alert(`Searching for ${keyword}`);
 
     //handling response from api and logging api response
     function handleResponse(response) {
       console.log(response.data[0]);
+      setResults(response.data[0]);
     }
 
     //documentation: https://dictionaryapi.dev/ making api call
@@ -29,6 +32,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
